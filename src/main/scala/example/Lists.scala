756f674f57faf984b53d13fa40df5f353c27b8c9
beauvill <charles.beauville@epfl.ch>
2019-09-19 16:35:55 +0200

    Fixed the sum and max functions of Lists

--- src/main/scala/example/Lists.scala
+++ src/main/scala/example/Lists.scala
@@ -22,7 +22,7 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = if xs.isEmpty then 0 else xs.head + sum(xs.tail)
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -37,5 +37,9 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = if xs.isEmpty 
+    then throw new NoSuchElementException 
+    else if xs.size == 1 then xs.head else compare(max(xs.tail), xs.head)
+
+  def compare(x: Int, y: Int): Int = if x >= y then x else y 
 }
